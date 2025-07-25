1c9d42182c68cc94e7ad1cf01ebdd8cd8d59b9a6
JhonatanRSantos <jhonatanatm@gmail.com>
2025-07-25 16:47:16 -0300

    Test golangci-lint workflow

--- main.go
+++ main.go
@@ -1,3 +1,11 @@
 package main
 
-func main() {}
+import "errors"
+
+func main() {
+	createError("This is an error message")
+}
+
+func createError(message string) error {
+	return errors.New(message)
+}
